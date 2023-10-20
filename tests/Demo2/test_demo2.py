c54c27d4a7ab609d3bcca5521e0b6c620a46b9f9
Tiarles Moralles Guterres <tiarles.moralles@typhoon-hil.com>
2023-10-20 15:35:27 +0200

    Small test upgrades WIP

--- tests/Demo2/test_demo2.py
+++ tests/Demo2/test_demo2.py
@@ -1,26 +1,27 @@
 import pytest
 
-
 @pytest.mark.polarion(test_id="DEMO-391")
 def test_if_true():
     assert 5 == 5
 
 
-@pytest.mark.polarion(test_id="DEMO-393")
+@pytest.mark.polarion(test_id="DEMO-392")
 def test_if_false():
     assert 5 == 6
 
 
-@pytest.mark.polarion(test_id="DEMO-394")
+@pytest.mark.polarion(test_id="DEMO-393")
 @pytest.mark.parametrize('value', [0, 10, 20, 25, 30])
 def test_with_parametrize(value):
-    assert value < 12 or value == 25
+    assert True
 
 
-@pytest.mark.polarion(test_id="DEMO-394")
+@pytest.mark.polarion(test_id="DEMO-398")
 @pytest.mark.parametrize('value', [0, 10, 20, 25, 30])
-def test_with_parametrize(value):
-    assert value < 12 or value == 25
+@pytest.mark.parametrize('value2', [0, 10, 20, 25, 30])
+@pytest.mark.parametrize('value3', [0, 10, 20, 25, 30])
+def test_with_parametrize(value, value2, value3):
+    assert value < value2 or value == value3
 
 
 # @pytest.mark.polarion(test_id="DEMO-654")
@@ -29,7 +30,26 @@ def test_with_parametrize(value):
 #      is not existent in Polarion'''
 #     assert 5 == 6
 
-
-@pytest.mark.polarion(test_id="DEMO-400")
+@pytest.mark.skip
+@pytest.mark.polarion(test_id="DEMO-394")
 def test_if_broken_block():
     assert np.array([1,2,3,4,5])
+
+@pytest.mark.skip
+def test_tmp():
+    from polarion.polarion import Polarion
+
+    POLARION_HOST='http://desktop-ajbsmkv/polarion/'
+    POLARION_USER='admin'
+    POLARION_PASSWORD='admin'
+
+    client = Polarion(polarion_url=POLARION_HOST,
+                    user=POLARION_USER,
+                    password=POLARION_PASSWORD)
+
+    project = client.getProject('Demo2')
+
+    test_case_id = "DEMO-391"
+    test_case_item = project.getWorkitem(test_case_id)
+
+    test_case_item.comments
